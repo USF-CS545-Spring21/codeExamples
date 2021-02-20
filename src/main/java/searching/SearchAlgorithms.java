fb749e8651d4318436585dc1152bc3e258cd2e4f
Olga Karpenko <okarpenko@usfca.edu>
2021-02-19 18:03:09 -0800

    Updated quick sort.

---
+++ src/main/java/searching/SearchAlgorithms.java
@@ -0,0 +1,23 @@
+package searching;
+
+public class SearchAlgorithms {
+
+    public static boolean linearSearch(int[] arr, int start, int elem) {
+
+        // fill in code
+        return false;
+    }
+
+    public static boolean binarySearch(int[] arr, int start, int end, int elem) {
+
+        int mid = (start + end) / 2;
+
+        // fill in code
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 10, 11, 25, 50, 67, 90};
+        binarySearch(arr, 0, arr.length - 1, 68);
+    }
+}
